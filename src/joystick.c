f200aa007177ee742364b7b4a0bcc251fecf3850
Adrien Friggeri <adrien@friggeri.net>
2024-02-26 22:30:13 +0000

    Only send values when they change

--- src/joystick.c
+++ src/joystick.c
@@ -18,6 +18,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 struct joystick_data {
   int16_t x;
   int16_t y;
+  int16_t prev_x;
+  int16_t prev_y;
 };
 
 static void input_handler(struct joystick_data *data, struct input_event *evt) {
@@ -26,26 +28,28 @@ static void input_handler(struct joystick_data *data, struct input_event *evt) {
   }
 
   switch (evt->code) {
-    case INPUT_ABS_X:
-      data->x = evt->value;
-      break;
-    case INPUT_ABS_Y:
-      data->y = evt->value;
-      break;
+  case INPUT_ABS_X:
+    data->x = evt->value;
+    break;
+  case INPUT_ABS_Y:
+    data->y = evt->value;
+    break;
   }
 
-  if (evt->sync) {
+  if (evt->sync && ((data->prev_x != data->x) || data->prev_y != data->y)) {
+    data->prev_x = data->x;
+    data->prev_y = data->y;
     zmk_hid_joystick_set((int8_t)data->x, (int8_t)data->y, 0);
     zmk_endpoints_send_joystick_report();
   }
 }
 
-#define JOYSTICK_INST(n)                                           \
-  static struct joystick_data data_##n = {};                       \
-  void input_handler_##n(struct input_event *evt) {                \
-    input_handler(&data_##n, evt);                                 \
-  }                                                                \
-  INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_INST_PHANDLE(n, device)), \
+#define JOYSTICK_INST(n)                                                       \
+  static struct joystick_data data_##n = {};                                   \
+  void input_handler_##n(struct input_event *evt) {                            \
+    input_handler(&data_##n, evt);                                             \
+  }                                                                            \
+  INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_INST_PHANDLE(n, device)),             \
                         input_handler_##n);
 
 DT_INST_FOREACH_STATUS_OKAY(JOYSTICK_INST)
